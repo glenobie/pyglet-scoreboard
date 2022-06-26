bbefdaae19ec440b9ca72d0cc06c115d8a428faa
glenobie <gwober@gmail.com>
2022-06-25 21:49:54 -0600

    first crack at recurive function for adding more nodes if games > 6

--- scoreboard_picker.py
+++ scoreboard_picker.py
@@ -1,5 +1,4 @@
 import socket
-import trace
 import pyglet
 from pyglet import font
 from key_handler import KeyHandler
@@ -68,28 +67,67 @@ class Carousel :
             self.backCenter = self.backCenter.getFront()
         
         size = len(self.scoreboards)
-        end = size - 1 # position of final item to be placed
+        end = size # position of final item to be placed
         if size > 0 :
-            self.listFront.setIndex(0)   
+            self.listFront.setIndex(0)   # always one in front
         if size > 1 :
-            self.listFront.getBack().setIndex(1)
+            self.listFront.getBack().setIndex(1) 
         if size > 2 :
             self.listFront.getFront().setIndex(2)
         if size > 3 and size % 2 == 0 : #put end of list in center back, if even number of items
             self.backCenter.setIndex(size-1)
             end -= 1
-        if size > 6 :
+        if size > 6 : # add 3 and 4 to the 'front'
             self.listFront.getBack().getBack().setIndex(3)
             self.listFront.getFront().getFront().setIndex(4)
-            self.scatterTheRemains(5, end, size) # inclusive
+            
+            # Now, 5 items in front, one in back if even number, and an even number of leftover items
+            # add half of those leftovers to each side
+
+            # find range to add to each side
+            # left range is 5 to  5 + number of items/2 - 1
+            numPerSide = (end - 5) / 2
+            leftmostNode = self.backCenter.getBack().getBack()
+            rightmostNode = self.backCenter.getFront().getFront()
+            self.scatterRemains(5, 5 + numPerSide - 1, leftmostNode, self.backCenter) 
+            self.scatterRemains(5 + numPerSide, end-1,  self.backCenter, rightmostNode)
         elif size == 5 :  # 4 or 5 items in list 
             self.backCenter.getBack().setIndex(3)
             self.backCenter.getFront().setIndex(4)
     
     # TODO: spread the remainder between back left and back right
-    def scatterTheRemains(self, start, end, size) :
-        0
+    # will always have an even number of elements to distribute
+    # always one node between leftNode and rightNode
+    def scatterRemains(self, startIndex, endIndex, leftNode, rightNode) :
+        # if odd number to place (1, 3, 5, etc) place one in gap node
+        # odd number to place if end - start is even
+        gapNode = leftNode.getFront()
+        if (endIndex - startIndex) % 2 == 0 :
+            gapNode.setIndex(startIndex)   
+            startIndex += 1
+             
+        # if more than 1 node left, need new nodes in gaps
+        if (endIndex - startIndex) > 0 : 
+            self.createNewNode(leftNode, gapNode)   
+            self.createNewNode(gapNode, rightNode)
+            nodesPerSide = (1 + endIndex - startIndex ) / 2
+            self.scatterRemains(startIndex, startIndex + nodesPerSide - 1, leftNode, gapNode)
+            self.scatterRemains(startIndex + nodesPerSide, endIndex, gapNode, rightNode)
 
+            # if odd number to place, place middle one in gap node,  recurse
+            
+    def createNewNode(self, leftNode, rightNode) :
+        location = self.calculateLocation(leftNode.getLocation(), rightNode.getLocation())
+        newNode = MenuLocationNode(location)
+        self.insertInFrontOf(leftNode, newNode)
+        
+        
+    # average position and scale between two points
+    def calculateLocation(self, leftLoc, rightLoc) :
+        x = (rightLoc[0] - leftLoc[0]) / 2
+        y = (rightLoc[1] - leftLoc[1]) / 2
+        s = (rightLoc[2] - leftLoc[2]) / 2
+        return (x, y, s)
 
     def insertInFrontOf(self, frontNode, newNode) :
         frontNode.getFront().setBack(newNode)
@@ -167,10 +205,9 @@ class ScoreboardPicker(KeyHandler) :
         self.configScreen = ConfigScreen(self.batch)
 
         # center points for the icons
-        # the list locationData will index into this list but only to the first 
-        # half
+
         self.controlPoints = ( (400, 240, 0.9),   # 0: front, center
-                               (220, 280, 0.6),   # 1: front, left of center
+                               (220, 260, 0.6),   # 1: front, left of center
                                (100, 320, 0.4),     # 2: front, just before apex
                                (80, 330, 0.4),     # 3: left apex
                                (100, 340, 0.35),    # 4: back, just after apex
@@ -180,10 +217,9 @@ class ScoreboardPicker(KeyHandler) :
                                (700, 340, 0.35),   # 8: back, just before apex
                                (720, 330, 0.4),    # 9: right apex
                                (700, 320, 0.4),    # 10: right, front, just after apex
-                               (580, 280, 0.6),    # 11: front, right of center
+                               (580, 260, 0.6),    # 11: front, right of center
                                )
 
-        # will contain lists of (index into gameList, index into controlPoints)
         self.handleEntry()
 
         
