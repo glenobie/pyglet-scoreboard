ee83a847bdb59fb6d096fb774839eb62c96a3b7c
glenobie <gwober@gmail.com>
2022-07-08 14:25:56 -0600

    autosave for football and basketball

--- basketball.py
+++ basketball.py
@@ -44,12 +44,35 @@ class BasketballGameState(TimedGameState) :
         else:
             self.teams[team].modifyTeamFouls(1)
 
+    def restoreFromList(self, stateList) :
+        self.seconds = int(stateList[0].strip('\n'))
+        self.period = int(stateList[1].strip('\n'))
+        self.teams[0].score = int(stateList[2].strip('\n'))
+        self.teams[0].teamFouls = int(stateList[3].strip('\n'))
+        self.teams[0].timeoutsTaken = int(stateList[4].strip('\n'))
+        self.teams[1].score = int(stateList[5].strip('\n'))
+        self.teams[1].teamFouls = int(stateList[6].strip('\n'))
+        self.teams[1].timeoutsTaken = int(stateList[7].strip('\n'))
+
+    def getStateAsList(self) :
+        stateList = []
+        stateList.append(str(self.seconds) +'\n')
+        stateList.append(str(self.period)+'\n')
+        stateList.append(str(self.teams[0].score)+'\n')
+        stateList.append(str(self.teams[0].teamFouls)+'\n')
+        stateList.append(str(self.teams[0].timeoutsTaken)+'\n')
+        stateList.append(str(self.teams[1].score) + '\n')
+        stateList.append(str(self.teams[1].teamFouls)+'\n')
+        stateList.append(str(self.teams[1].timeoutsTaken)+'\n')
+        return stateList
+
+
 ####################################
 class BasketballScoreboard(Scoreboard) :
     def __init__(self) :
-        Scoreboard.__init__(self)
         self.state = BasketballGameState()
-        
+        Scoreboard.__init__(self)
+         
         self.addScores(3, 470)
         self.addClock(440)
         self.addPeriod(300)
