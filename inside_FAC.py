7dca7c6d3f03cc1d40670d8b162c39b315645a83
glenobie <gwober@gmail.com>
2022-07-10 13:39:08 -0600

    more inside crease facs

--- inside_FAC.py
+++ inside_FAC.py
@@ -5,13 +5,17 @@ from fac_set import FACSet
 from pyglet import font
 
 class FACField() :
-    F_LIGHT_BLUE = (0, 255, 255)
+    F_BLUE = (0, 255, 255)
     F_YELLOW = (255,255,150)
     F_GRAY = (180,180,180)
+    F_WHITE = (240,240,240)
+    F_GREEN = (102, 255, 102)
     B_BLACK = (0,0,0)
+
 
     FONT_SIZE = 20
     FONT_COLOR = (0, 0, 0, 255)
+    LINE_SPACING = 6
 
 
     def __init__(self, bgColor, width, height, text='', fillFunc=None, 
@@ -27,8 +31,8 @@ class FACField() :
                                                        font_size=FACField.FONT_SIZE,
                                                        font_name=FACSet.TEXT_FONT,
                                                        align='center',
-                                                       line_spacing=FACField.FONT_SIZE+2))
-        self.layout = pyglet.text.layout.TextLayout(self.doc, multiline=True, width=100, batch=batch, group=fgGroup)
+                                                       line_spacing=FACField.FONT_SIZE+FACField.LINE_SPACING))
+        self.layout = pyglet.text.layout.TextLayout(self.doc, multiline=True, width=width, batch=batch, group=fgGroup)
         self.layout.anchor_x = 'left'
         self.layout.anchor_y = 'bottom'    
         #self.layout.content_valign = 'bottom'
@@ -38,38 +42,69 @@ class FACField() :
 
         #center layout in border
         x = left #+ (self.border.width - self.layout.content_width) / 2
-        y = bottom +  (self.border.height - self.layout.content_height + 2) // 2 
+        y = bottom +  (self.border.height - self.layout.content_height + FACField.LINE_SPACING) // 2 
         self.layout.position = (x, y)
+
+    def setText(self, text) :
+        self.doc.text = text
+
 
 class InsideSportsSet(FACSet) :
 
-    FIELD_WIDTH = 100
+    FIELD_WIDTH = 128
     FIELD_HEIGHT = 61
     SPACE = 5
-    FONT = 'Showcard Gothic'
+    
+    TEXT_FIELD_COLOR = FACField.F_GRAY
  
-    LEFT = [(FACField.F_GRAY, 1, 'PEN'), (FACField.F_GRAY, 1, 'LOOSE\nPUCK'),
-                 (FACField.F_GRAY, 1, 'REB'), (FACField.F_GRAY, 1, 'SHOTS'),
-                 (FACField.F_LIGHT_BLUE, 3, ''), (FACField.F_LIGHT_BLUE, 3, ''),
-                 (FACField.F_GRAY, 1, 'PASS\nTO')]
+    # (color, width, title, index into list of card fields)
+    COLUMNS = [ ( (FACField.F_GRAY, 1, 'PEN'), (FACField.F_GRAY, 1, 'LOOSE PUCK'),
+                  (FACField.F_GRAY, 1, 'REB'), (FACField.F_GRAY, 1, 'SHOTS'),
+                  (FACField.F_BLUE, 3, 'FLIP'), (FACField.F_BLUE, 3, 'FLIP'),
+                  (FACField.F_GRAY, 1, 'PASS TO')), 
+                ( (FACField.F_WHITE, 2, 'FLIP'), (FACField.F_WHITE, 2, 'FLIP'),
+                  (FACField.F_YELLOW, 2, 'FLIP'), (FACField.F_GREEN, 2, 'FLIP'),
+                  None, None, (FACField.F_BLUE, 1, 'FLIP')  ),
+                ( None, None, None, None, None, None, (FACField.F_BLUE, 1, 'FLIP') ),
+                ( (FACField.F_WHITE, 2, 'FLIP'), (FACField.F_WHITE, 2, 'FLIP'),
+                    (FACField.F_YELLOW, 1, 'FLIP'), (FACField.F_GREEN, 3, 'FLIP'),
+                    (FACField.F_WHITE, 1, 'FLIP'), (FACField.F_GRAY, 1, 'FO'),
+                    (FACField.F_BLUE, 2, 'FLIP')),
+                ( None, None, (FACField.F_GRAY, 1, 'REB'), None, 
+                 (FACField.F_WHITE, 1, 'FLIP'), (FACField.F_GRAY, 1, 'AST'), None),
+                ( (FACField.F_GRAY, 1, 'MAJOR\nMINOR'), (FACField.F_GRAY, 1, 'DUMP INS'),
+                  (FACField.F_WHITE, 1, 'FLIP'), None, (FACField.F_WHITE, 1, 'FLIP'), 
+                  (FACField.F_GRAY, 1, 'BREAK AWAY'), (FACField.F_GRAY, 1, 'PASS TO')
+                ) ]
 
     def __init__(self, loader) :
         FACSet.__init__(self, loader)
         self.fg = pyglet.graphics.OrderedGroup(62)
         self.FACbg = pyglet.graphics.OrderedGroup(5)
 
+        # test
+        self.index = 1
 
         self.paintBackground(FACSet.W_COLOR_WHITE)
 
         # test
-        self.fields = []
-        y = InsideSportsSet.SPACE
-        for spot in InsideSportsSet.LEFT :
-            f = FACField(spot[0], InsideSportsSet.FIELD_WIDTH * spot[1], InsideSportsSet.FIELD_HEIGHT, 
-                        spot[2], batch=self.batch, fgGroup=self.fg, bgGroup=self.FACbg)
-            f.setPosition(InsideSportsSet.SPACE, y)
-            y += InsideSportsSet.FIELD_HEIGHT + InsideSportsSet.SPACE
-            self.fields.append(f)
+        self.textFields = []
+        self.valueFields = []
+        x = InsideSportsSet.SPACE
+        for col in InsideSportsSet.COLUMNS :
+            y = InsideSportsSet.SPACE           
+            for spot in col :
+                if not(spot is None) :
+                    f = FACField(spot[0], InsideSportsSet.FIELD_WIDTH * spot[1] + InsideSportsSet.SPACE * (spot[1] - 1), 
+                                InsideSportsSet.FIELD_HEIGHT, spot[2], batch=self.batch, fgGroup=self.fg, 
+                                bgGroup=self.FACbg)
+                    f.setPosition(x, y)
+                    if spot[0] == InsideSportsSet.TEXT_FIELD_COLOR :
+                        self.textFields.append(f)
+                    else :
+                        self.valueFields.append(f)
+                y += InsideSportsSet.FIELD_HEIGHT + InsideSportsSet.SPACE
+            x += InsideSportsSet.FIELD_WIDTH + InsideSportsSet.SPACE
 
 
 
@@ -78,7 +113,9 @@ class InsideSportsSet(FACSet) :
         self.batch.draw()
 
     def flipFAC(self) :
-        0
+        for f in self.valueFields :
+            f.setText(str(self.index))
+            self.index += 1
 
     def handle_L(self) :
         self.flipFAC()
