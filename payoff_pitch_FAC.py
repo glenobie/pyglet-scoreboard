0676a2762c97fe0b13c6ec369dcfaa9c80a4c4ef
Glen Oberhauser <gwober@gmail.com>
2022-07-25 16:11:29 -0600

    more payoff pitch

--- payoff_pitch_FAC.py
+++ payoff_pitch_FAC.py
@@ -2,7 +2,7 @@ import pyglet
 from die import Die
 from bordered_dice_set import BorderedDiceSet
 from fac_set import FACSet
-
+from functools import partial
 
 class PayoffPitchSet(FACSet) :
 
@@ -24,8 +24,8 @@ class PayoffPitchSet(FACSet) :
         p1 = Die(Die.D_WHITE, Die.T_BLACK, 6, self.batch)
         p2 = Die(Die.D_WHITE, Die.T_BLACK, 6, self.batch)
         d = [p1,p2]
-        for br in d :
-            br.scale(0.8)
+        for die in d :
+            die.scale(0.8)
 
 
         self.pitchSet = BorderedDiceSet(d, batch= self.batch)
@@ -38,7 +38,27 @@ class PayoffPitchSet(FACSet) :
 
         self.hitSet = BorderedDiceSet([h1], batch= self.batch)
         self.hitSet.setTitle('Hit')
+        self.hitSet.attachBooleanFunctionLabel((partial(self.hitSet.totalEquals, 100), "Rare?"))
+        self.hitSet.attachBooleanFunctionLabel((partial(self.hitSet.totalEquals, 30), "WP?"))
+        self.hitSet.attachBooleanFunctionLabel((partial(self.hitSet.totalEquals, 31), "WP?"))
+        self.hitSet.attachBooleanFunctionLabel((partial(self.hitSet.totalEquals, 32), "WP?"))
+        self.hitSet.attachBooleanFunctionLabel((partial(self.hitSet.totalEquals, 33), "WP?"))
+        self.hitSet.attachBooleanFunctionLabel((partial(self.hitSet.totalEquals, 34), "WP?"))
+        self.hitSet.attachBooleanFunctionLabel((partial(self.hitSet.totalEquals, 35), "WP?"))
         self.hitSet.setPosition(360, 400, 10)
+
+        dp1 = Die(Die.D_GREEN, Die.T_WHITE, 6, self.batch)
+        dp2 = Die(Die.D_GREEN, Die.T_WHITE, 6, self.batch)
+        dp = [dp1,dp2]
+        for die in dp :
+            die.scale(0.8)
+
+
+        self.dpSet = BorderedDiceSet(dp, batch= self.batch)
+        self.dpSet.setTitle('DP?')
+        self.dpSet.attachBooleanFunctionLabel((self.always, self.dpSet.totalAsString))
+        self.dpSet.setPosition(60, 220, 10)
+
 
     def draw(self) :
         self.batch.draw()
@@ -48,6 +68,8 @@ class PayoffPitchSet(FACSet) :
 
     def handle_L(self) :
         self.pitchSet.roll()
+        self.hitSet.roll()
+        self.dpSet.roll()
 
 
     def handle_K(self) :
