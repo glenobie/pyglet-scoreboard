c8ae13019c11bbe8ec100ac66f2bc64f4cb41c8d
Glen Oberhauser <gwober@gmail.com>
2022-08-01 16:55:24 -0600

    IP Fac fields

--- inside_paint_FAC.py
+++ inside_paint_FAC.py
@@ -41,6 +41,7 @@ class InsidePaintSet(FACSet) :
     def __init__(self, loader) :
         FACSet.__init__(self, loader)
         self.paintBackground(FACSet.W_COLOR_WHITE)
+        self.positions = ['C', 'PF', 'F', 'SG', 'PG']
         self.fg = pyglet.graphics.OrderedGroup(62)
         self.FACbg = pyglet.graphics.OrderedGroup(5)
         self.createLayout()
@@ -74,11 +75,42 @@ class InsidePaintSet(FACSet) :
     def draw(self) :
         self.batch.draw()
 
+    # returns a list of 3 pass To values
+    def getPassTo(self) :
+        texts = ['','','']
+        p = random.random()
+        random.shuffle(self.positions)
+        texts[0] = self.positions[0]
+
+        if (p > 0.5) :
+            texts[0] += ' '
+            value = random.randint(1,20)
+            if value <= 12 :
+                texts[0] += '*'
+            texts[0] += '(' + str(value) + ')'
+            texts[1] = self.positions[1] + ' (' + str(11 - value % 10) + ')'
+            if value >= 17 :
+                texts[2] = 'Any ' + str(random.randint(21, 30) )
+
+
+        return texts
+        # 20 for each position
+        # each position (1-20), with (1-12) asterisked
+ 
+        # function -> second number = (11 - first % 10 )
+        # if first >= 17, third elemnt is 'Any ' + a random 21-30
 
     def generateFAC(self) :
         random.shuffle(self.positions)
-  
+        self.valueFields[16].setText('# '+str(random.randint(1,40)))
+        self.valueFields[15].setText('# '+str(random.randint(1,40)))
+        self.valueFields[11].setText('Offensive Rebound\n# '+str(random.randint(1,40)))
+        passes = self.getPassTo()
+        self.valueFields[4].setText(passes[0])
+        self.valueFields[8].setText(passes[1])
+        self.valueFields[10].setText(passes[2])
 
+    
 
     def handle_L(self) :
         self.generateFAC()
