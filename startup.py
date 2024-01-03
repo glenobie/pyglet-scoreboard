d63f24f12e6c8d4b2f25a7f9a0f37206690145b4
Glen Oberhauser <gwober@gmail.com>
2024-01-03 14:05:50 -0700

    again

--- startup.py
+++ startup.py
@@ -7,7 +7,7 @@ from game_picker import MainWindow
 ################
 
 isPi = socket.gethostname() == "raspberrypi"
-window = MainWindow(800, 1200, isPi)
+window = MainWindow(800, 1100, isPi)
 window.activate() # should work on Linux, won't on windows
 pyglet.clock.schedule_interval(window.update, 1/60.0)
 pyglet.app.run()
